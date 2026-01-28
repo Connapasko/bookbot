8ec4fe2f7d29f44b830fcdb9013453911d1568e9
connapasko <connapasko@gmail.com>
2026-01-28 01:19:55 -0500

    added sort_char_counts into stats.py for ch3 lesson - saving due to quit mid chapter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from stats import count_words_in_book, count_each_char
+from stats import count_words_in_book, count_each_char, sort_char_counts
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -10,7 +10,15 @@ def get_book_text(filepath):
 
 def main():
     word_input = get_book_text("books/frankenstein.txt")
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     count_words_in_book(word_input)
-    print(count_each_char(word_input))
+    print("--------- Character Count -------")
+    char_counts = count_each_char(word_input)
+    sorted_chars = sort_char_counts(char_counts)
+    for item in sorted_chars:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,19 @@ def count_each_char(booktext):
         else:
             char_count[lowered_char] = 1
     return char_count
+
+
+def sort_on_num(items):
+    return items["num"]
+
+def sort_char_counts(char_counts):
+    char_list = []
+    for char in char_counts:
+        if char.isalpha():
+            char_list.append({
+                "char":char,
+                "num": char_counts[char]
+            
+            })
+    char_list.sort(reverse=True, key=sort_on_num )
+    return (char_list)
